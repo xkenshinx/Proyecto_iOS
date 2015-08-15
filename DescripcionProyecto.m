7c0a3f7a046d626493e697e3958de1a656dd69fc
Kenneth Chacon V <kenshin@Stevens-Mac.local>
2015-08-15 17:40:16 -0600

    Descripcion del Proyecto
    Upload Detalle del Proyecto

---
+++ DescripcionProyecto.m
@@ -0,0 +1,18 @@
+Como les comente, el Proyecto Final va a ser crear un "Spotify Streamer", el cual debe de permitir hacer lo siguiente:
+Realizar una búsqueda por nombre de artistas y desplegar la lista junto con la foto de los artistas.
+Cuando el usuario seleccione un artista, se debe de mostrar un listado con el top de canciones, junto con la imagen del album al que pertenece la canción.
+Cuando el usuario seleccione una canción, la misma debe de reproducirse abriendo una ventana nueva para el reproductor, la cual debe tener los siguientes controles:
+Play: reproducir canción
+Pause: Pausar canción
+Next: Ir a la siguiente canción de la lista
+Previous: Ir a la canción anterior de la lista
+Recuerden que para ello van a necesitar usar el API de Spotify (aquí la pueden encontrar) y para eso necesitan registrarse y registrar una aplicación para que obtengan un API Key.
+
+Deben de enviarme el código usando GitHub y para eso, les paso este pequeño tutorial:
+Parte 1
+Parte 2
+Como recordarán, este proyecto deben enviarlo en la última clase, pero para no solo esperar a simplemente tener una nota al final, vamos a ir creando avances en forma de Tareas, las cuales van a valer del 20% del valor del proyecto. Este es el cronograma para estos días que no nos vamos a ver:
+Agosto 15: Storyboard del App (Sin AutoLayout). Usen el Archivo adjunto como práctica o referencia.
+Agosto 22: Storyboard del App CON AutoLayout. Pueden usar este documento para que practiquen.
+Agosto 29: Listado de Artistas y Búsqueda
+Estas tareas deben de ser entregadas antes de las 11:59:59 pm (Hora Gmail) de cada uno de los días mencionados. Si la entregan después es como si no la hubieran hecho.
